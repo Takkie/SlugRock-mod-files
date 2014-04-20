c25bd3dc7fac19865eac9701a8a74c813848a17e
flesk <flesk88@gmail.com>
2014-04-20 21:20:16 +0200

    Load the mod with sv_pure 0 @ SlugRock-ioq3.bat

--- SlugRock-ioq3.bat
+++ SlugRock-ioq3.bat
@@ -1,2 +1,2 @@
 cd ..
-ioquake3.x86.exe +set fs_game "SlugRock" +set vm_cgame 2 +set vm_game 2 +set vm_ui 2
+ioquake3.x86.exe +set fs_game "SlugRock" +set vm_cgame 2 +set vm_game 2 +set vm_ui 2 +set sv_pure 0
